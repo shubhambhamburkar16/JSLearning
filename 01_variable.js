e630c24cc1c593fd2df458433a20d6f696dc393a
shubhambhamburkar16 <shubhambhamburkar16@gmail.com>
2023-09-20 00:31:42 -0400

    create 01_variableAsssign.js
    create 01_variableAsssign.js

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Shubham";  // Variable initialization
+console.log(firstName);
+firstName = "Mr. Shubham"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Bhamburkar"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
---
+++ 01_variableAssign.js
@@ -0,0 +1,23 @@
+
+var myFullName; //variable declaration
+console.log(myFullName);
+myFullName = "Shubham Bhamburkar"; //varible initialization
+console.log(myFullName);
+myFullName = "Shubham Dhanraj Bhamburkar"; // variable update
+console.log(myFullName);
+var myLove = "I love only JS"; //variable declaration and initialization
+console.log(myLove);
+var firstName = "Shubham"; // variable declaration
+console.log("First Name:",firstName);
+var lastName = "Bhamburkar";
+console.log("Last Name:",lastName);
+var age = "21";
+console.log("Age:",age);
+var address = "Vihigaon bk";
+console.log("Address:",address);
+var pincode = "444705";
+console.log("Pincode:",pincode);
+var gender = "Male";
+console.log("Gender:",gender);
+var isMarried = "NA";
+console.log("IsMarried:",isMarried);
