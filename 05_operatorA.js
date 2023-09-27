afdb89b86ed096630db6cda7b994b9e47dedc32a
shubhambhamburkar16 <shubhambhamburkar16@gmail.com>
2023-09-27 04:37:43 -0400

    create operator A & B.js file
    create operator A & B.js file

---
+++ 05_operatorA.js
@@ -0,0 +1,70 @@
+console.log(`*******************Assignment - 01 ********************88`);
+function squareOfWordLength(str1){
+
+}
+squareOfWordLength("Javascirpt");
+console.log(`1.The Given String Name is = "Javascript"`);
+var str1 = "Javascript";
+var totalLength = str1.length;
+console.log(`1.1 Total length of the string is: ${str1.length}`);
+function square(num){
+   var result = num * num; 
+   return result;
+}
+var squareNum = square(10);
+console.log(`1.2   Num is 10 and its square is: ${squareNum}`);
+console.log(`----------------------------------------------------------------------------`);
+function squareOfWordLength(str2){
+
+}
+squareOfWordLength("Google Chrome");
+console.log(`2. The Given String Name is = "Google Chrome"`);
+var str2 = "Google Chrome";
+var totalLength = str2.length;
+console.log(`2.1  length of the string is: ${str2.length}`);
+function square(num){
+   var result = num * num; 
+   return result;
+}
+var squareNum = square(13);
+console.log(`2.2 Num is 13 and its square is: ${squareNum}`);
+console.log(`-----------------------------------------------------------------------------------`);
+function squareOfWordLength(str3){
+
+}
+squareOfWordLength("Developer Smart");
+console.log(`3. The Given String Name is = "Developer Smart"`);
+var str3 = "Developer Smart";
+var totalLength = str3.length;
+console.log(`3.1  Total length of the string is: ${str3.length}`);
+function square(num){
+   var result = num * num; 
+   return result;
+}
+var squareNum = square(15);
+console.log(`3.2  Num is 15 and its square is: ${squareNum}`);
+console.log(`------------------------------------------------------------------`);
+function developer(){
+console.log(`4.  The Given String Name is: "I am Angular Developer"`);
+}
+developer();
+var string4 = "I am Angular Developer";
+var totalLength = string4.length;
+console.log(`4.1  Total length of the given String is: ${string4.length}`);
+var totalWords = string4.split(" ");
+console.log(`4.2  Total Number of Words in the Given String are= ${totalWords.length}`);
+function divide(num){
+    var result = 22 / 4; 
+    return result;
+ }
+ var divideNum = divide();
+ console.log(`4.3  The Dividation of the Given String is: ${divideNum}`);
+
+ function multiply(num){
+    var result = 22 * 4; 
+    return result;
+ }
+ var multiplyNum = multiply();
+ console.log(`4.4   The Multiplication of the Given String is: ${multiplyNum}`);
+  console.log(`------------------------------------End---------------------------------------------------`);
+
---
+++ 05_operatorB.js
@@ -0,0 +1,61 @@
+console.log(`*********************** Assignment - 02 ***************************8`);
+console.log(`==========Step 1===========`);
+function greaterNumber(value1,value2){
+
+}
+greaterNumber();
+var num1 = 10;
+var num2 = -10;
+console.log(`The Given Number is: 10, -10`);
+var result = num1> num2? num1 :num2;
+console.log(`The Greater Number is: ${result}`);
+
+
+console.log(`The Given Number is: 800, 899`);
+var num3 = 800;
+var num4 = 899;
+var result = num3> num4? num3 :num4;
+console.log(`The Greater Number is: ${result}`);
+console.log(`---------------------------------------------------------------------------------------`);
+console.log(`==================Step2==================`);
+function EvenOrOddNum(){
+
+}
+EvenOrOddNum();
+console.log(`The Given Number is: 29`);
+var num1 = 29;
+var result = num1%2==0? "Even" : "Odd";
+console.log(`The Given Number is : ${result}`);
+console.log(`The Given Number is: 44`);
+var num2 = 44;
+var result = num2%2==0? "Even" : "Odd";
+console.log(`The Given Number is : ${result}`);
+console.log(`The Given Number is: 0`);
+var num3 = 0;
+var result = num3%2==0? "Even" : "Odd";
+console.log(`The Given Number is : ${result}`);
+console.log(`The Given Number is: 101`);
+var num4 = 101;
+
+var result = num4%2==0? "Even" : "Odd";
+console.log(`The Given Number is : ${result}`);
+console.log(`----------------------------------------------------------------------------------------`);
+console.log(`================================Step 3=======================`);
+var str1 = "Javascript";
+var totalLength = str1.length;
+var string1 = totalLength;
+var result = string1%2==0? "Even Length" : "Odd Length";
+console.log(`The Given Function "Javascript" is: ${result}`); 
+
+var str2 = "developer";
+var totalLength = str2.length;
+var string2 = totalLength;
+var result = string2%2==0? "Even Length" : "Odd Length";
+console.log(`The Given Function 'developer' is: ${result}`); 
+
+var str3 = "Google";
+var totalLength = str3.length;
+var string3 = totalLength;
+var result = string3%2==0? "Even Length" : "Odd Length";
+console.log(`The Given Function "Google" is: ${result}`); 
+console.log(`--------------------End-----------------------------`);
