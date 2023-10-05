ebb73804f67296d6d473127b9f41e5049a9133c4
shubhambhamburkar16 <shubhambhamburkar16@gmail.com>
2023-10-04 23:16:04 -0400

    update count.js
    update count.js

--- count.js
+++ count.js
@@ -1,29 +1,20 @@
 console.log(`============Assignment - Count Character===========`);
-var str1 ="I Am Learning Javascript, The Language of internet";
+function countCharA(arg1){
+    console.log(`The Character present in the given string== ${arg1}`);
+var str1 =arg1;
 var count = 0;
     for (let index = 0; index < str1.length-1; index++) {
         var char = str1.charAt(index);
         if (char=='A' || char=='a') {
             count = count + 1;
         }
-        
     }
-
-console.log(`The Given String is : ${str1}`);
-console.log(`Total Number of Character 'A' & 'a' present in Given String : ${count}`) ;
+    console.log(`Total Number of Character 'A' & 'a' present in Given String : ${count}`) ;
+}
+countCharA('I AM Learning Javascript, The Language of internet');
 console.log(`---------------------------------------------------------------------------------------------------`);
-var str2 ="My favourite movie is LAggAn";
-var count = 0;
- 
-    for (let index = 0; index < str2.length-1; index++) {
-        var char = str2.charAt(index);
-        if (char=='A' || char=='a') {
-            count = count + 1;
-        }
-        
-    }
-console.log(`The Given String is : ${str2}`);
-console.log(`Total Number of Character 'A' & 'a' present in Given String : ${count}`) ;
+countCharA('My favourite movie is LAggAn');
+console.log(`---------------------------------End--------------------------------`);
 
 
 
