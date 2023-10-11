c92be0207c86639c99617968c24a9551e99763d8
shubhambhamburkar16 <shubhambhamburkar16@gmail.com>
2023-10-10 18:57:40 -1200

    update both Assignment
    update both Assignment

--- 09_date.js
+++ 09_date.js
@@ -5,7 +5,18 @@
 
 const date = new Date();
 console.log(date);
-
+console.log(date.getDate());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+var dt = new Date("Aug 23, 2023 24:40:00");
+console.log(dt);
+var d = new Date("2023-03-25");
+console.log(d);
+var dd= new Date("03/25/2024");
+console.log(dd);
+const ddd = new Date('20 Sep 2023');
+console.log(ddd);
+console.log(`====================================================`);
 const square10 = Math.pow(10,2);
 console.log(square10);
 
--- 09_factorial.js
+++ 09_factorial.js
@@ -23,7 +23,23 @@ function factorial(num){
 }
 
 factorial(5);
-
 factorial(7);
-
 factorial(10);
+factorial(23);
+console.log(`=================== Check whether the given Number is Prime or Not===================`);
+const isPrime = num=> {
+  const boundary = Math. floor(Math.sqrt(num));
+  for (let index = 2; index <= boundary; index++) 
+  if (num % index===0) 
+  return false; 
+  return num >= 2;
+    
+  };
+  console.log(`The Number 11 is ${isPrime(11)}`);
+  console.log(`The Number 23 is ${isPrime(23)}`);
+  console.log(`The Number 31 is ${isPrime(31)}`);
+  console.log(`The Number 44 is ${isPrime(44)}`);
+  console.log(`The Number 56 is ${isPrime(56)}`);
+  
+
+
